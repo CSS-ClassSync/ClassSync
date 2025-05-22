faae847737758756923c5159b4ab60632ed4d9d7
Andrei Aldea <aaldea.977@gmail.com>
2025-05-22 09:18:08 +0300

    ClassSync.Tests: add coverage reports

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     application
+    java
+    jacoco
 }
 
 repositories {
@@ -12,10 +14,41 @@ dependencies {
 
     testImplementation("org.junit.jupiter:junit-jupiter:5.10.0")
     testImplementation("org.mockito:mockito-core:5.12.0")
+
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+jacoco {
+    toolVersion = "0.8.10"
+}
+
+tasks.jacocoTestReport {
+//    dependsOn(tasks.test)
+    dependsOn(tasks.test)
+    reports {
+        xml.required.set(true)
+        csv.required.set(false)
+        html.required.set(true)
+        html.outputLocation.set(layout.buildDirectory.dir("reports/jacoco"))
+    }
+    executionData.from(fileTree(layout.buildDirectory).include("/jacoco/*.exec"))
+
+    onlyIf {
+        executionData.files.any { it.exists() }
+    }
 }
 
 tasks.test {
     useJUnitPlatform()
+
+    testLogging {
+        events("passed", "skipped", "failed")
+    }
+    // Generate test results even if tests fail
+    ignoreFailures = true
+
+    // Still finalize with JaCoCo report
+    finalizedBy(tasks.jacocoTestReport)
 }
 
 
@@ -23,5 +56,7 @@ java { toolchain { languageVersion = JavaLanguageVersion.of(21) } }
 
 application {
     mainClass = "io.github.ClassSyncCSS.ClassSync.UI.ClassSync"
+
+
 }
 
